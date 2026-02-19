ec64323894700646c96ac2edbe9359155ffc60fc
Giorgio Garofalo <giogar2705@gmail.com>
2026-02-18 23:30:31 -0800

    Setup globals

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Zilla Slab',
+        cssVariable: '--font-body',
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'Odor Mean Chey',
+        cssVariable: '--font-heading',
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'Urbanist',
+        cssVariable: '--font-sans',
+      },
+    ],
+  },
+});
