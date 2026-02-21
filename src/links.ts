5cc78d569f05ed2a639c7a0ca1ab7111bde322ff
Giorgio Garofalo <giogar2705@gmail.com>
2026-02-20 19:49:46 -0800

    Add CTA to installation

---
+++ src/links.ts
@@ -0,0 +1,6 @@
+const REPO = 'https://github.com/iamgio/quarkdown';
+
+export const links = {
+  gettingStarted: `${REPO}?tab=readme-ov-file#getting-started`,
+  learnMore: `${REPO}?tab=readme-ov-file#table-of-contents`,
+} as const;
