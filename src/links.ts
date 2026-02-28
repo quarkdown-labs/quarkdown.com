6da3db1db7b56e14de1befc8a1f95e52fcedecfc
Giorgio Garofalo <giogar2705@gmail.com>
2026-02-28 11:27:30 -0800

    Add footer navigation

--- src/links.ts
+++ src/links.ts
@@ -2,7 +2,9 @@ const REPO = 'https://github.com/iamgio/quarkdown';
 
 export const links = {
   wiki: '/wiki',
+  quickstart: '/quickstart',
   docs: '/docs',
+  vsCode: '/vs-code',
   github: REPO,
   gettingStarted: `${REPO}?tab=readme-ov-file#getting-started`,
   learnMore: `${REPO}?tab=readme-ov-file#table-of-contents`,
