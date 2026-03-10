102d8a35ae64c47c1f03b59538de7d58aec5fa6e
Giorgio Garofalo <giogar2705@gmail.com>
2026-03-09 21:15:27 -0700

    Add json-ld

--- src/links.ts
+++ src/links.ts
@@ -1,12 +1,13 @@
 const REPO = 'https://github.com/iamgio/quarkdown';
 
 export const links = {
+  base: 'https://quarkdown.com',
   wiki: '/wiki',
   quickstart: '/wiki/quickstart',
   docs: '/docs/quarkdown-stdlib',
   vsCode: '/vs-code',
   discussions: `${REPO}/discussions`,
-  github: REPO,
+  repo: REPO,
   gettingStarted: `${REPO}?tab=readme-ov-file#getting-started`,
   learnMore: `${REPO}?tab=readme-ov-file#table-of-contents`,
 } as const;
---
+++ src/meta.ts
@@ -0,0 +1,12 @@
+export const meta = {
+  type: 'SoftwareApplication',
+  name: 'Quarkdown',
+  description:
+    'Quarkdown is a modern, open-source, Markdown-based typesetting system ' +
+    'for creating papers, presentations, knowledge bases and static websites. ',
+  author: 'Giorgio Garofalo',
+  themeColor: '#061111',
+  appCategory: 'DeveloperApplication',
+  appOS: 'Cross-platform',
+  license: 'https://opensource.org/licenses/GPL-3.0',
+} as const;
